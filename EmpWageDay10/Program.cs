908b455677f13de53826834f64f14c595e11fdba
Aj007ar <aj007.ar@gmail.com>
2022-08-20 18:52:50 +0530

    [Ajay] Added Employee Wage

---
+++ EmpWageDay10/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpWageDay10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee Wage program");
+        }
+    }
+}
