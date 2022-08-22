dc10f446b9ba1c1e3a0d58afa6aafe321bd034e9
Aj007ar <aj007.ar@gmail.com>
2022-08-22 16:47:50 +0530

    [Ajay] Added UC1 Employee Wage

---
+++ EmpWageDay10/UC1EmplyeeAttendance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageDay10
+{
+    internal class UC1EmplyeeAttendance
+    {
+        public void CheckAttendance()
+        {
+            int Full_Time = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == 1)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
