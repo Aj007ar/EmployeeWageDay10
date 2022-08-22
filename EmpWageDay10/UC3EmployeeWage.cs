5fdbafbc761830a0c4846c0334cc93b07a5bba1a
Aj007ar <aj007.ar@gmail.com>
2022-08-22 16:51:57 +0530

    [Ajay] Added UC3 Employee Wage

---
+++ EmpWageDay10/UC3EmployeeWage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageDay10
+{
+    internal class UC3EmployeeWage
+    {
+        public void DailyWage()
+        {
+            int Full_Time = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == 1)
+            {
+                int empRatePerHr = 20;
+                int empHour = 8;
+                int salary = (empRatePerHr * empHour);
+                Console.WriteLine("Employee present for full day then salary is: " + salary);
+            }
+            else
+            {
+                int salary = 0;
+                Console.WriteLine("Employee is Absent then salary is:" + salary);
+            }
+        }
+    }
+}
